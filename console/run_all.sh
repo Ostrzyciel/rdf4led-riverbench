c9e41ecd2bbf82c19ad2628888fada96a17287b1
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2024-05-07 13:27:23 +0200

    Update benchmark scripts

--- console/run_all.sh
+++ console/run_all.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
 EXP_DIR="$1"
-DATASET="$2"
 
-ENGINES=(
-  'jena eu.ostrzyciel.rb_load_time.benchmark.jenatdb.JenaTdbInput eu.ostrzyciel.rb_load_time.jenatdb'
-  'rdf4j_native eu.ostrzyciel.rb_load_time.benchmark.rdf4j.RDF4JInput eu.ostrzyciel.rb_load_time.rdf4j'
-  'rdf4j_lmdb eu.ostrzyciel.rb_load_time.benchmark.rdf4j.RDF4JInput eu.ostrzyciel.rb_load_time.rdf4j'
-  'virtuoso eu.ostrzyciel.rb_load_time.benchmark.virtuoso.VirtuosoInput eu.ostrzyciel.rb_load_time.virtuoso'
-  'rdf4led eu.ostrzyciel.rb_load_time.benchmark.rdf4led.RDF4LedInput eu.ostrzyciel.rb_load_time.rdf4led'
+DATASETS=(
+  'assist-iot-weather'
+  'citypulse-traffic'
+  'dbpedia-live'
+  'digital-agenda-indicators'
+  'linked-spending'
+  'lod-katrina'
+  'muziekweb'
+  'politiquices'
 )
 
-docker pull ghcr.io/ostrzyciel/rdf4led-riverbench:main
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for engine in "${ENGINES[@]}"; do
-  IFS=' ' read -r -a engine <<< "$engine"
-  docker run -it --rm \
-    -v "$(realpath "$EXP_DIR")/data:/exp/data" \
-    -v "$(realpath "$EXP_DIR")/result:/exp/result" \
-    -v "$(realpath "$EXP_DIR")/store:/exp/store" \
-    ghcr.io/ostrzyciel/rdf4led-riverbench:main \
-    java -cp "./${engine[2]}/*:./${engine[2]}/libs/*" \
-      "${engine[1]}" \
-      "config/${engine[0]}_${DATASET}.json"
-
-  echo "Cleaning up ${engine[0]} store"
-  rm -rf "$EXP_DIR/store/${engine[0]}"
-  echo "Done."
+for dataset in "${DATASETS[@]}"; do
+  bash "${__dir}/run_one_dataset.sh" "$EXP_DIR" "$dataset"
 done
---
+++ console/run_one_dataset.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+EXP_DIR="$1"
+DATASET="$2"
+
+ENGINES=(
+  'jena eu.ostrzyciel.rb_load_time.benchmark.jenatdb.JenaTdbInput eu.ostrzyciel.rb_load_time.jenatdb'
+  'rdf4j_native eu.ostrzyciel.rb_load_time.benchmark.rdf4j.RDF4JInput eu.ostrzyciel.rb_load_time.rdf4j'
+  'rdf4j_lmdb eu.ostrzyciel.rb_load_time.benchmark.rdf4j.RDF4JInput eu.ostrzyciel.rb_load_time.rdf4j'
+  'virtuoso eu.ostrzyciel.rb_load_time.benchmark.virtuoso.VirtuosoInput eu.ostrzyciel.rb_load_time.virtuoso'
+  'rdf4led eu.ostrzyciel.rb_load_time.benchmark.rdf4led.RDF4LedInput eu.ostrzyciel.rb_load_time.rdf4led'
+)
+
+echo "Pulling the docker image"
+docker pull ghcr.io/ostrzyciel/rdf4led-riverbench:main
+
+echo "Extracting the dataset: $DATASET"
+tar -zxf "$EXP_DIR/data/$DATASET.tgz" -C "$EXP_DIR/data" --strip-components=1
+echo "Done."
+
+for engine in "${ENGINES[@]}"; do
+  IFS=' ' read -r -a engine <<< "$engine"
+  docker run -it --rm \
+    -v "$(realpath "$EXP_DIR")/data:/exp/data" \
+    -v "$(realpath "$EXP_DIR")/result:/exp/result" \
+    -v "$(realpath "$EXP_DIR")/store:/exp/store" \
+    ghcr.io/ostrzyciel/rdf4led-riverbench:main \
+    java -cp "./${engine[2]}/*:./${engine[2]}/libs/*" \
+      "${engine[1]}" \
+      "config/${engine[0]}_${DATASET}.json"
+
+  echo "Cleaning up ${engine[0]} store"
+  rm -rf "$EXP_DIR/store/${engine[0]}"
+  echo "Done."
+done
+
+echo "Cleaning up the dataset: $DATASET"
+rm -rf "$EXP_DIR/data/$DATASET"
+echo "All done."
